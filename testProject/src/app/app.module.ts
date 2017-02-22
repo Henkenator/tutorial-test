72514e9431983c63d42ff229c4e6bdbe1f705a6e
Henkenator <ljunggren.henrik@gmail.com>
2017-02-22 03:34:28 +0100

    added generic table

--- testProject/src/app/app.module.ts
+++ testProject/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GenericTableModule } from 'angular2-generic-table';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    GenericTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
